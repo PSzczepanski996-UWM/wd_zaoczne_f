287dbf1c3ffcb3a537a480f252b1182ce977e973
PSzczepanski996-UWM <p.szczepanski996@gmail.com>
2020-02-29 11:59:35 +0100

    'Koniec'

---
+++ start.py
@@ -0,0 +1,17 @@
+print('Hello world!')
+
+def main():
+    pass
+
+# łańcuchy znaków
+liczba = 'Ala'
+liczba = 0
+liczba = 0.0
+# PEP8
+imie = 'Ala'
+print(imie)
+print(type(imie))
+print(type(5))
+
+imie.lower()
+print(imie)
